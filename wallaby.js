79a174cc1f402d9a17dcb2d773433374e2c90e31
Andrew Tan <andrewvmail@gmail.com>
2018-07-29 15:30:02 -0700

    finally working!

--- wallaby.js
+++ wallaby.js
@@ -4,7 +4,6 @@ var compilerOptions = Object.assign(
 
 compilerOptions.module = 'CommonJs';
 
-const process = require('ts-jest/preprocessor.js').process;
 const TEMPLATE_URL_REGEX = /templateUrl\s*:\s*('|")(\.\/){0,}(.*)('|")/g;
 const STYLE_URLS_REGEX = /styleUrls\s*:\s*\[[^\]]*\]/g;
 const ESCAPE_TEMPLATE_REGEX = /(\${|\`)/g;
@@ -12,6 +11,7 @@ const ESCAPE_TEMPLATE_REGEX = /(\${|\`)/g;
 module.exports = function (wallaby) {
   return {
     files: [
+      'src/**/*.html',
       {pattern: 'src/**/*.+(ts|css|less|scss|sass|styl|html|json|svg)', load: true},
       {pattern: 'src/**/*.d.ts', ignore: true},
       {pattern: 'src/**/*spec.ts', ignore: true}
@@ -22,41 +22,25 @@ module.exports = function (wallaby) {
     env: { type: "node", runner: "node" }, testFramework: "ava", debug: true,
 
     compilers: {
-      '**/*.ts': wallaby.compilers.typeScript(compilerOptions)
+      '**/*.ts': wallaby.compilers.typeScript(compilerOptions),
     },
     preprocessors: {
+      '**/*.ts': (file, done) => {
+        // from https://github.com/thymikee/jest-preset-angular/blob/master/setupJest.js
+        file.content = file.content
+          .replace(TEMPLATE_URL_REGEX, 'template: require($1./$3$4)')
+          .replace(STYLE_URLS_REGEX, 'styles: []');
+        done(file.content + '//modified')
+      },
       '**/*.html': (file, done) => {
-
-        // file.content = file.content.replace(ESCAPE_TEMPLATE_REGEX, '\\$1');
-        //
-        // file.content = file.content
-        //   .replace(TEMPLATE_URL_REGEX, 'template: require($1./$3$4)')
-        //   .replace(STYLE_URLS_REGEX, 'styles: []');
-        //
-        // done(file.content + '// modified')
-        // return process(src, path, config, transformOptions);
-
-      }
-      // '**/*.jsx': file => require('babel')
-      //   .transform(file.content, {sourceMap: true, filename: file.path})
+        if(file.path.endsWith('index.html')) {
+          return done(file.content)
+        }
+        file.content = file.content.replace(ESCAPE_TEMPLATE_REGEX, '\\$1');
+        file.content = 'module.exports=`' + file.content  + '`;'
+        done(file.content + '//modified')
+      },
     },
-
-    // middleware: function (app, express) {
-    //   var path = require('path');
-    //   app.use('/favicon.ico', express.static(path.join(__dirname, 'src/favicon.ico')));
-    //   app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
-    // },
-
-    // env: {
-    //   kind: 'node'
-    // },
-
-    // postprocessor: webpackPostprocessor,
-
-    setup: function () {
-      // window.__moduleBundler.loadTests();
-    },
-
     debug: true
   };
 };
